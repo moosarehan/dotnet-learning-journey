fcd2691c2e7284107312536a12dab7acb9fa271f
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:55:55 +0500

    Add User entity class for account authentication

---
+++ Entities/User.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace myfirstrestapi.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string? name { get; set; }
+        public string?email { get; set; }
+        public string? password { get; set; }
+
+
+
+    }
+}
