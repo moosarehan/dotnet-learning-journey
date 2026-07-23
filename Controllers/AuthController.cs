be94b1c50a45ea18f18c74cc9c40c924e06c90c8
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:46:07 +0500

    Add AuthController with authentication endpoints

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myfirstrestapi.Dto;
+using myfirstrestapi.GenericResponse;
+using myfirstrestapi.IServices;
+namespace myfirstrestapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser([FromBody] UserDto user)
+        {
+            try
+            {
+                var result = await authService.LoginUser(user);
+                if (result.Item1 == 0)
+                {
+                    return NotFound(Response<UserDto>.failure(null, result.Item2));
+                }
+              
+                return Ok(Response<UserDto>.success(user, "Login successful"));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserDto user)
+        {
+            try
+            {
+                var result = await authService.register(user);
+                if (result.Item1 == 0)
+                {
+                    return BadRequest(Response<UserDto>.failure(null, result.Item2));
+                }
+                return Ok(Response<UserDto>.success(user, "Register successful"));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
