0899964329c33008306e08afaf7ed40fc69f0341
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:55:31 +0500

    Add UserDto data transfer object for authentication

---
+++ Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myfirstrestapi.Dto
+{
+    public class UserDto
+    {
+       
+        public Guid id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+    }
+}
