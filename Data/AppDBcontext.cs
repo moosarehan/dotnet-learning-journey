d221bb2a135bf0649068014198d9a7480acd68f2
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:55:13 +0500

    Add AppDBcontext database context class

---
+++ Data/AppDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfirstrestapi.Entities;
+
+namespace myfirstrestapi.Data
+{
+    public class AppDBcontext:DbContext
+    {
+        public AppDBcontext(DbContextOptions<AppDBcontext> options):base(options)
+        {
+
+                
+        }
+
+
+        public DbSet<User> AccountUser { get; set; }
+        public DbSet<Employe> Employees { get; set; }
+
+    }
+}
