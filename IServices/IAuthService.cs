f8d2a6d271909b8b13a36aa3256890f53dae3da4
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:58:21 +0500

    Add IAuthService interface for authentication operations

---
+++ IServices/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using myfirstrestapi.Dto;
+
+namespace myfirstrestapi.IServices
+{
+    public interface IAuthService
+    {
+        Task<Tuple<int, string>> LoginUser(UserDto user);
+        Task<Tuple<int, string>> register(UserDto user);
+    }
+}
