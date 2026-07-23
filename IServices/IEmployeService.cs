c4dcc06469fb801f6de84917cd133ca678dbdb0d
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:58:28 +0500

    Add IEmployeService interface for employee management

---
+++ IServices/IEmployeService.cs
@@ -0,0 +1,16 @@
+﻿using myfirstrestapi.Dto;
+using myfirstrestapi.Entities;
+
+namespace myfirstrestapi.IServices
+{
+    public interface IEmployeService
+    {
+        Task<Tuple<int, List<Employedto>>> getallemployees();
+        Task<Tuple<int, string, Employe?>> createEmploye(Employedto e);
+        Task<Tuple<int, string, Employe?>> UpdateEmployee(Employedto employee);
+
+        Task<Tuple<int, string>> DeleteEmployee(Guid id);
+
+        Task<Tuple<int, Employedto?>> GetEmployeeById(Guid id);
+    }
+}
