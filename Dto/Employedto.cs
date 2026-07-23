e61db4e4151afd2f383ada5f62f982f8f46a8302
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:55:44 +0500

    Add Employedto data transfer object for employee management

---
+++ Dto/Employedto.cs
@@ -0,0 +1,16 @@
+﻿namespace myfirstrestapi.Dto
+{
+    public class Employedto
+    {
+
+        public Guid id { get; set; } 
+        public string? name { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? modifieddate { get; set; }
+        public DateOnly? DOB { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? position { get; set; }
+        public string? department { get; set; }
+    }
+}
