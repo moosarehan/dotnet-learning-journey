e4ca981d1a3000e816d794c9e9e5cb0bcb5c0d11
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:58:10 +0500

    Add Response generic response wrapper class

---
+++ GenericResponse/Response.cs
@@ -0,0 +1,28 @@
+﻿namespace myfirstrestapi.GenericResponse
+{
+    public class Response<T>
+    {
+        public T? data { get; set; }
+        public bool Status { get; set; }
+        public string? Message { get; set; }  
+        public static Response<T> success(T? data,string msg)
+        {
+            return new Response<T>
+            {
+                data = data,
+                Status = true,
+                Message = msg
+            };
+        }
+
+        public static Response<T> failure(T?data, string msg)
+        {
+            return new Response<T>
+            {
+                data = data,
+                Status = false,
+                Message = msg
+            };
+        }
+    }
+}
