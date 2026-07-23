99689c9e1b015b764c68c742b57c764066af44ae
moosarehan <moosarehan4@gmail.com>
2026-07-23 18:56:06 +0500

    Add Employe entity class for employee data model

---
+++ Entities/Employe.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myfirstrestapi.Entities
+{
+    public class Employe
+    {
+        [Key]
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string? name { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        
+        public DateTime? modifieddate { get; set; }
+        public DateOnly? DOB { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? position { get; set; }
+        public string? department { get; set; }
+    }
+}
