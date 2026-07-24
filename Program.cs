fb6d1cbbe946220a928f390a20578d7308218ba6
moosarehan <moosarehan4@gmail.com>
2026-07-24 17:31:07 +0500

    feat: configure JWT Bearer authentication and add UseAuthentication middleware

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using myfirstrestapi.Data;
 using myfirstrestapi.IServices;
 using myfirstrestapi.Services;
@@ -8,6 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDBcontext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "http://localhost:5115",
+        ValidAudience = "http://localhost:5115",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("QtGin2leKfRhbQlvv9JzF9krMMSYIIy13PaRhN0tBc8"))
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -22,6 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
